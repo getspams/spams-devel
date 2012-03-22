9062da9a2fedb66be39ded46884728f0b834cbc0
mairal <mairal@5c5caf25-3c07-451e-92dc-633d67ccb328>
2012-03-22 17:15:25 +0000

    correction doc mexBayer
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/spams-devel/trunk@10 5c5caf25-3c07-451e-92dc-633d67ccb328

--- src_release/mexBayer.m
+++ src_release/mexBayer.m
@@ -1,10 +1,8 @@
 % 
 % Usage:   Y =mexBayer(X,offset);
 %
-% Name: mexBayer applies a Bayer pattern to an image X.
+% Description: mexBayer applies a Bayer pattern to an image X.
 %       There are four possible offsets.
-%
-% Description: 
 %
 % Inputs: X:  double m x n matrix   
 %         offset: scalar, 0,1,2 or 3   
