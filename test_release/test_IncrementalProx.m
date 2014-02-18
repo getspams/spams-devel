16417c166beb40de84c259ca7f596697c43a89af
mairal <mairal@5c5caf25-3c07-451e-92dc-633d67ccb328>
2014-02-18 11:19:08 +0000

    toto
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/spams-devel/trunk@146 5c5caf25-3c07-451e-92dc-633d67ccb328

--- test_release/test_IncrementalProx.m
+++ test_release/test_IncrementalProx.m
@@ -32,12 +32,11 @@ param.numThreads=-1;    % uses all possible cores
 param.normalized=false;  % if the columns of X have unit norm, set to true.
 param.strategy=1;        % MISO with all heuristics
                          % 0: no heuristics, slow  (only for comparison purposes)
-                         % 1: adjust the constant L on 5% of the data  (good for non-strongly convex problems)
+                         % 1: MISO1: adjust the constant L on 5% of the data  (good for non-strongly convex problems)
                          % 2: adjust the constant L on 5% of the data + unstable heuristics (this strategy does not work)
-                         % 3: adjust the constant L on 5% of the data + stable heuristic good for strongly-convex problems
-                         % 4: best for l2 regularization
+                         % 3: MISO2: adjust the constant L on 5% of the data + stable heuristic good for strongly-convex problems
+                         % 4: MISOmu: best for l2 regularization
 param.verbose=true;
-param.minibatches=min(n,ceil(1/density));  % size of the minibatches, requires to store twice the size of X 
 
 % set grid of lambda
 max_lambda=max(abs(X*y))/n;
@@ -50,6 +49,8 @@ param.lambda=tablambda;
 fprintf('EXPERIMENT: ALL LAMBDAS WITHOUT WARM RESTART\n');
 param.warm_restart=false;
 param.verbose=false;
+param.strategy=1;
+param.minibatches=min(n,ceil(1/density));  % size of the minibatches, requires to store twice the size of X  with strategy=1
 obj=[];
 spar=[];
 for ii=1:length(tabepochs)
@@ -100,6 +101,8 @@ y=sign(y);
 param.regul='l2';        % many other regularization functions are available
 param.loss='logistic';     % only square and log are available
 param.num_threads=-1;    % uses all possible cores
+param.strategy=4;        
+param.minibatches=1; % with strategy=4, minibatches is better set to 1
 %param.strategy=3;        % MISO with all heuristics
 fprintf('EXPERIMENT: ALL LAMBDAS WITHOUT WARM RESTART\n');
 param.warm_restart=false;
@@ -120,10 +123,17 @@ for ii=1:length(tabepochs)
    obj
 end
 
-fprintf('EXPERIMENT: SEQUENTIAL LAMBDAS WITH WARM RESTART\n');
-fprintf('A SINGLE CORE IS USED\n');
-param.warm_restart=true;
-param.num_threads=1;
+
+fprintf('EXPERIMENT FOR LOGISTIC REGRESSION + l2\n');
+y=sign(y);
+param.regul='l2';        % many other regularization functions are available
+param.loss='logistic';     % only square and log are available
+param.num_threads=-1;    % uses all possible cores
+param.strategy=3;         % MISO2
+param.minibatches=min(n,ceil(1/density));  % size of the minibatches, requires to store twice the size of X 
+fprintf('EXPERIMENT: ALL LAMBDAS WITHOUT WARM RESTART\n');
+param.warm_restart=false;
+param.verbose=false;
 obj=[];
 spar=[];
 for ii=1:length(tabepochs)
@@ -134,8 +144,9 @@ for ii=1:length(tabepochs)
    tic
    [Beta tmp]=mexIncrementalProx(y,X,Beta0,param);
    toc
+   yR=repmat(y,[1 nlambdas]);
    fprintf('Objective functions: \n');
-   obj=[obj; tmp(1,:)];
+   obj=[obj;tmp(1,:)];
    obj
 end
 
@@ -148,7 +159,8 @@ fprintf('EXPERIMENT FOR LOGISTIC REGRESSION + l1\n');
 param.regul='l1';        % many other regularization functions are available
 param.loss='logistic';     % only square and log are available
 param.num_threads=-1;    % uses all possible cores
-%param.strategy=3;        % MISO with all heuristics
+param.strategy=1;  
+param.minibatches=min(n,ceil(1/density));  % size of the minibatches, requires to store twice the size of X 
 fprintf('EXPERIMENT: ALL LAMBDAS WITHOUT WARM RESTART\n');
 param.warm_restart=false;
 param.verbose=false;
@@ -168,29 +180,4 @@ for ii=1:length(tabepochs)
    obj=[obj;tmp(1,:)];
    obj
 end
-
-fprintf('EXPERIMENT: SEQUENTIAL LAMBDAS WITH WARM RESTART\n');
-fprintf('A SINGLE CORE IS USED\n');
-param.warm_restart=true;
-param.num_threads=1;
-obj=[];
-spar=[];
-for ii=1:length(tabepochs)
-   param.epochs=tabepochs(ii);   % one pass over the data
-   fprintf('EXP WITH %d PASS\n',tabepochs(ii));
-   nlambdas=length(param.lambda);
-   Beta0=zeros(p,nlambdas);
-   tic
-   [Beta tmp]=mexIncrementalProx(y,X,Beta0,param);
-   toc
-   fprintf('Objective functions: \n');
-   obj=[obj; tmp(1,:)];
-   obj
-   fprintf('Sparsity: \n');
-   spar=[spar; sum(Beta ~= 0)];
-   spar
-end
-
-
-
 
