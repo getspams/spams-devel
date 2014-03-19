71eda5d132924bc96ed3749e753900d672dd2938
chieze <chieze@5c5caf25-3c07-451e-92dc-633d67ccb328>
2014-03-19 11:58:20 +0000

    mkdist : .png files were not correctly copied text_dictlearn : python interface files have been regenerated
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/spams-devel/trunk@153 5c5caf25-3c07-451e-92dc-633d67ccb328

--- swig/python/text_dictlearn/viewdict_gui.py
+++ swig/python/text_dictlearn/viewdict_gui.py
@@ -2,21 +2,26 @@
 
 # Form implementation generated from reading ui file 'viewdict_gui.ui'
 #
-# Created: Thu Jul 11 11:11:28 2013
-#      by: PyQt4 UI code generator 4.7.2
+# Created: Wed Mar 19 12:40:11 2014
+#      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
 class Ui_DictWindow(object):
     def setupUi(self, DictWindow):
-        DictWindow.setObjectName("DictWindow")
+        DictWindow.setObjectName(_fromUtf8("DictWindow"))
         DictWindow.resize(950, 714)
         self.centralwidget = QtGui.QWidget(DictWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.gridLayout = QtGui.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.dictviewbb = QtGui.QFrame(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -26,17 +31,17 @@ class Ui_DictWindow(object):
         self.dictviewbb.setMaximumSize(QtCore.QSize(16777215, 50))
         self.dictviewbb.setFrameShape(QtGui.QFrame.StyledPanel)
         self.dictviewbb.setFrameShadow(QtGui.QFrame.Raised)
-        self.dictviewbb.setObjectName("dictviewbb")
+        self.dictviewbb.setObjectName(_fromUtf8("dictviewbb"))
         self.horizontalLayout = QtGui.QHBoxLayout(self.dictviewbb)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.DVnode_var = QtGui.QComboBox(self.dictviewbb)
-        self.DVnode_var.setObjectName("DVnode_var")
-        self.DVnode_var.addItem("")
-        self.DVnode_var.addItem("")
-        self.DVnode_var.addItem("")
+        self.DVnode_var.setObjectName(_fromUtf8("DVnode_var"))
+        self.DVnode_var.addItem(_fromUtf8(""))
+        self.DVnode_var.addItem(_fromUtf8(""))
+        self.DVnode_var.addItem(_fromUtf8(""))
         self.horizontalLayout.addWidget(self.DVnode_var)
         self.DVall = QtGui.QPushButton(self.dictviewbb)
-        self.DVall.setObjectName("DVall")
+        self.DVall.setObjectName(_fromUtf8("DVall"))
         self.horizontalLayout.addWidget(self.DVall)
         self.DVClose = QtGui.QPushButton(self.dictviewbb)
         palette = QtGui.QPalette()
@@ -69,10 +74,10 @@ class Ui_DictWindow(object):
         palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, brush)
         self.DVClose.setPalette(palette)
         font = QtGui.QFont()
-        font.setWeight(75)
         font.setBold(True)
+        font.setWeight(75)
         self.DVClose.setFont(font)
-        self.DVClose.setObjectName("DVClose")
+        self.DVClose.setObjectName(_fromUtf8("DVClose"))
         self.horizontalLayout.addWidget(self.DVClose)
         self.DVHelp = QtGui.QPushButton(self.dictviewbb)
         palette = QtGui.QPalette()
@@ -114,10 +119,10 @@ class Ui_DictWindow(object):
         palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, brush)
         self.DVHelp.setPalette(palette)
         font = QtGui.QFont()
-        font.setWeight(75)
         font.setBold(True)
+        font.setWeight(75)
         self.DVHelp.setFont(font)
-        self.DVHelp.setObjectName("DVHelp")
+        self.DVHelp.setObjectName(_fromUtf8("DVHelp"))
         self.horizontalLayout.addWidget(self.DVHelp)
         self.gridLayout.addWidget(self.dictviewbb, 0, 0, 1, 1)
         self.VDframe = QtGui.QFrame(self.centralwidget)
@@ -128,17 +133,17 @@ class Ui_DictWindow(object):
         self.VDframe.setSizePolicy(sizePolicy)
         self.VDframe.setFrameShape(QtGui.QFrame.StyledPanel)
         self.VDframe.setFrameShadow(QtGui.QFrame.Raised)
-        self.VDframe.setObjectName("VDframe")
+        self.VDframe.setObjectName(_fromUtf8("VDframe"))
         self.verticalLayout = QtGui.QVBoxLayout(self.VDframe)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.gridLayout.addWidget(self.VDframe, 1, 0, 1, 1)
         DictWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(DictWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 950, 19))
-        self.menubar.setObjectName("menubar")
+        self.menubar.setObjectName(_fromUtf8("menubar"))
         DictWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(DictWindow)
-        self.statusbar.setObjectName("statusbar")
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
         DictWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(DictWindow)
