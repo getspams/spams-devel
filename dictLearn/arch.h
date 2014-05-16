f924e055002938286382b1cc45c84b5516e6b747
yuachen <yuachen@5c5caf25-3c07-451e-92dc-633d67ccb328>
2014-05-16 22:08:10 +0000

    Put all functions into 2 general functions archetypalAnalysisContinue and archetypalAnalysis. Change alphaArchAs to decompSimplex
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/spams-devel/trunk@171 5c5caf25-3c07-451e-92dc-633d67ccb328

--- dictLearn/arch.h
+++ dictLearn/arch.h
@@ -48,7 +48,7 @@ void archContinueForAS(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, co
 template <typename T>
 void archForAS(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5));
 
-/// Same implementation with memorizing XtX during iterstions
+/// Same implementation with memorizing XtX during iterations
 
 template <typename T>
 void archContinueForASMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5));
@@ -82,13 +82,33 @@ template <typename T>
 void archForCombined(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5));
 
 template <typename T>
+void archContinueForCombinedMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5));
+
+template <typename T>
+void archForCombinedMemo(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5));
+
+template <typename T>
 void archRobustContinueForCombined(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3));
 
 template <typename T>
 void archRobustForCombined(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3));
 
 template <typename T>
-void alphaArchAS(const Matrix<T>& X, const Matrix<T>& Z, SpMatrix<T>& alpha); 
+void archRobustContinueForCombinedMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3));
+
+template <typename T>
+void archRobustForCombinedMemo(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3));
+
+/// General functions including previous ones. Less parameters and simple use, for Python and Matlab interface
+
+template <typename T>
+void archetypalAnalysisContinue(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const bool robust = true, const T epsilon2 = T(10e-3), const bool computeXtX = false, const int stepsFISTA = 5, const int stepsAS = 50);
+
+template <typename T>
+void archetypalAnalysis(const Matrix<T>& X, const int p, Matrix<T>& Z, const bool robust = true, const T epsilon2 = T(10e-3), const bool computeXtX = false, const int stepsFISTA = 5, const int stepsAS = 50);
+
+template <typename T>
+void decompSimplex(const Matrix<T>& X, const Matrix<T>& Z, SpMatrix<T>& alpha, const bool computerZtZ = false); 
 
 /* **************************
  *  Implementations 
@@ -189,6 +209,9 @@ void archContinueForASMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z
   const int n = X.n();
   const int p = Z0.n();
   Z.copy(Z0);
+  Matrix<T> GX;
+  X.XtX(GX);
+  GX.addDiag(lambda2*lambda2);
   Matrix<T> G;
   Matrix<T> AlphaT(p,n);
   Matrix<T> BetaT(n,p);
@@ -240,7 +263,7 @@ void archContinueForASMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z
         matRSS.mult(copRowAlphaT, vBarre, 1/sumAsq, T());
         // least square to get Beta
         BetaT.refCol(l, refColBetaT);
-        activeSet<T>(X, vBarre, refColBetaT, lambda2, epsilon, warm);
+        activeSetS<T>(X, vBarre, refColBetaT, GX, lambda2, epsilon, warm);
         X.mult(refColBetaT, refColZ);
       }
     }
@@ -578,6 +601,131 @@ void archForCombined(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1
 }
 
 template <typename T>
+void archContinueForCombinedMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5)) {
+  const int m = X.m();
+  const int n = X.n();
+  const int p = Z0.n();
+  Z.copy(Z0);
+  Matrix<T> GX;
+  X.XtX(GX);
+  GX.addDiag(lambda2*lambda2);
+  Matrix<T> G;
+  Matrix<T> AlphaT(p,n);
+  Matrix<T> BetaT(n,p);
+  T RSS = -1.0;
+  Vector<T> refColX;
+  Vector<T> refColAlphaT;
+  Vector<T> refColZ;
+  Vector<T> copRowAlphaT;
+  Vector<T> refColBetaT;
+  Matrix<T> matRSS(m,n);
+  Vector<T> vBarre(m);
+  Vector<T> norms;
+  cout.precision(8);
+
+  for(int t=0; t<I1; ++t) {
+    // step 1: fix Z to compute Alpha
+    for(int i=0; i<n; ++i) {
+      X.refCol(i,refColX);
+      AlphaT.refCol(i, refColAlphaT);
+      gpFISTAFor(Z,refColX, refColAlphaT, T(1.0), T(1.0/0.7), 50, true);
+    }
+    // step 2: fix Alpha, fix all but one to compute Zi
+    for(int l=0; l<p; ++l) {
+      AlphaT.copyRow(l, copRowAlphaT);
+      T sumAsq =  copRowAlphaT.nrm2sq();
+      Z.refCol(l, refColZ);
+      // matRSS = X- Z*AlphaT
+      matRSS.copy(X);
+      Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+      if(sumAsq < T(10e-8)) {
+        // singular
+        matRSS.norm_2_cols(norms);
+        int k = norms.max();
+        X.refCol(k, refColX);
+        refColZ.copy(refColX);
+      } else {
+        matRSS.rank1Update(refColZ, copRowAlphaT);
+        matRSS.mult(copRowAlphaT, vBarre, 1/sumAsq, T());
+        // least square to get Beta
+        BetaT.refCol(l, refColBetaT);
+        gpFISTAFor(X, vBarre, refColBetaT, T(1.0), T(1.0/0.7), 50, true);
+        X.mult(refColBetaT, refColZ);
+      }
+    }
+
+    matRSS.copy(X);
+    Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+    RSS = matRSS.normF();
+    cout << "RSS FISTA = " << RSS << endl;
+  }  
+
+  for(int t=0; t<I2; ++t) {
+    // step 1: fix Z to compute Alpha
+    // memorize ZtZ
+    Z.XtX(G);
+    G.addDiag(lambda2*lambda2);
+    for(int i=0; i<n; ++i) {
+      X.refCol(i,refColX);
+      AlphaT.refCol(i, refColAlphaT);
+      activeSetS<T>(Z,refColX, refColAlphaT, G, lambda2, epsilon, warm);
+    }
+    // step 2: fix Alpha, fix all but one to compute Zi
+    for(int l=0; l<p; ++l) {
+      AlphaT.copyRow(l, copRowAlphaT);
+      T sumAsq =  copRowAlphaT.nrm2sq();
+      Z.refCol(l, refColZ);
+      // matRSS = X- Z*AlphaT
+      matRSS.copy(X);
+      Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+      if(sumAsq < T(10e-8)) {
+        // singular
+        matRSS.norm_2_cols(norms);
+        int k = norms.max();
+        X.refCol(k, refColX);
+        refColZ.copy(refColX);
+      } else {
+        matRSS.rank1Update(refColZ, copRowAlphaT);
+        matRSS.mult(copRowAlphaT, vBarre, 1/sumAsq, T());
+        // least square to get Beta
+        BetaT.refCol(l, refColBetaT);
+        activeSetS<T>(X, vBarre, refColBetaT, GX, lambda2, epsilon, warm);
+        X.mult(refColBetaT, refColZ);
+      }
+    }
+
+    matRSS.copy(X);
+    Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+    RSS = matRSS.normF();
+    cout << "RSS AS = " << RSS << endl;
+  }
+}
+
+template <typename T>
+void archForCombinedMemo(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5)) {
+  const int m = X.m();
+  const int n = X.n();
+  Matrix<T> Z0(m,p);
+  Vector<T> refColZ0;
+  Vector<T> refColX;
+  if(!randominit ) {
+    for(int i=0; i<p; i++) {
+      X.refCol(i%n, refColX);
+      Z0.refCol(i%n, refColZ0);
+      refColZ0.copy(refColX);
+    }
+  } else {
+    for(int i=0; i<p; i++) {
+      int k = random() % n;
+      X.refCol(k, refColX);
+      Z0.refCol(i, refColZ0);
+      refColZ0.copy(refColX);
+    }
+  }
+  archContinueForCombinedMemo(X, Z0, Z, I1, I2, warm, lambda2, epsilon);
+}
+
+template <typename T>
 void archRobustContinueForCombined(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3)) {
   const int m = X.m();
   const int n = X.n();
@@ -721,18 +869,222 @@ void archRobustForCombined(const Matrix<T>& X, const int p, Matrix<T>& Z, const 
 }
 
 template <typename T>
-void alphaArchAS(const Matrix<T>& X, const Matrix<T>& Z, SpMatrix<T>& alpha) {
+void archRobustContinueForCombinedMemo(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3)) {
+  const int m = X.m();
+  const int n = X.n();
+  const int p = Z0.n();
+  Z.copy(Z0);
+  Matrix<T> GX;
+  X.XtX(GX);
+  GX.addDiag(lambda2*lambda2);
+  Matrix<T> G;
+  Matrix<T> AlphaT(p,n);
+  Matrix<T> BetaT(n,p);
+
+  T RSN = -1.0;
+  Vector<T> refColX;
+  Vector<T> refColAlphaT;
+  Vector<T> refColZ;
+  Vector<T> copRowAlphaT;
+  Vector<T> refColBetaT;
+  Matrix<T> matRSS(m,n);
+  Vector<T> vBarre(m);
+  Vector<T> norms;
+  cout.precision(8);
+
+  for(int t=0; t<I1; ++t) {
+    // step 1: fix Z to compute Alpha
+    for(int i=0; i<n; ++i) {
+      X.refCol(i,refColX);
+      AlphaT.refCol(i, refColAlphaT);
+      gpFISTAFor(Z, refColX, refColAlphaT, T(1.0), T(1.0/0.7), 10, true);
+    }
+    // update scale factors
+    matRSS.copy(X);
+    Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+    matRSS.norm_2_cols(norms);
+    norms.thrsmax(epsilon2);
+    norms.Sqrt();
+    // step 2: fix Alpha, fix all but one to compute Zi
+    for(int l=0; l<p; ++l) {
+      Z.refCol(l, refColZ);
+      
+      AlphaT.copyRow(l, copRowAlphaT);
+      copRowAlphaT.div(norms);
+      T sumAsq =  copRowAlphaT.nrm2sq();
+      
+      matRSS.copy(X);
+      Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+
+      if(sumAsq < T(10e-8)) {
+        // singular
+        matRSS.norm_2_cols(norms);
+        int k = norms.max();
+        X.refCol(k, refColX);
+        refColZ.copy(refColX);
+      } else {
+        // absorbe the weights by rowAlphaT
+        copRowAlphaT.div(norms);
+        matRSS.mult(copRowAlphaT, vBarre, 1/sumAsq, T());
+        vBarre.add(refColZ);
+        // least square to get Beta
+        BetaT.refCol(l, refColBetaT);
+        gpFISTAFor(X, vBarre, refColBetaT, T(1.0), T(1.0/0.7), 10, true); 
+        X.mult(refColBetaT, refColZ);
+      }
+    }
+
+    matRSS.copy(X);
+    Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+    matRSS.norm_2_cols(norms);
+    RSN = norms.sum();
+    cout << "RSN FISTA= " << RSN << endl;
+  }
+
+  for(int t=0; t<I2; ++t) {
+    // step 1: fix Z to compute Alpha
+    Z.XtX(G);
+    G.addDiag(lambda2*lambda2); 
+    for(int i=0; i<n; ++i) {
+      X.refCol(i,refColX);
+      AlphaT.refCol(i, refColAlphaT);
+      activeSetS<T>(Z,refColX, refColAlphaT, G, lambda2, epsilon, warm);
+    }
+    // update scale factors
+    matRSS.copy(X);
+    Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+    matRSS.norm_2_cols(norms);
+    norms.thrsmax(epsilon2);
+    norms.Sqrt();
+    // step 2: fix Alpha, fix all but one to compute Zi
+    for(int l=0; l<p; ++l) {
+      Z.refCol(l, refColZ);
+      
+      AlphaT.copyRow(l, copRowAlphaT);
+      copRowAlphaT.div(norms);
+      T sumAsq =  copRowAlphaT.nrm2sq();
+      
+      matRSS.copy(X);
+      Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+
+      if(sumAsq < T(10e-8)) {
+        // singular
+        matRSS.norm_2_cols(norms);
+        int k = norms.max();
+        X.refCol(k, refColX);
+        refColZ.copy(refColX);
+      } else {
+        // absorbe the weights by rowAlphaT
+        copRowAlphaT.div(norms);
+        matRSS.mult(copRowAlphaT, vBarre, 1/sumAsq, T());
+        vBarre.add(refColZ);
+        // least square to get Beta
+        BetaT.refCol(l, refColBetaT);
+        activeSetS<T>(X, vBarre, refColBetaT, GX, lambda2, epsilon, warm);
+        X.mult(refColBetaT, refColZ);
+      }
+    }
+
+    matRSS.copy(X);
+    Z.mult(AlphaT, matRSS, false, false, T(-1.0), T(1.0));
+    matRSS.norm_2_cols(norms);
+    RSN = norms.sum();
+    cout << "RSN AS= " << RSN << endl;
+  }
+}
+
+template <typename T>
+void archRobustForCombinedMemo(const Matrix<T>& X, const int p, Matrix<T>& Z, const int I1 = 3, const int I2 = 20, const bool randominit = false, const bool warm = false, const T lambda2 = T(10e-5), const T epsilon = T(10e-5), const T epsilon2 = T(10e-3)) {
+  const int m = X.m();
+  const int n = X.n();
+  Matrix<T> Z0(m,p);
+  Vector<T> refColZ0;
+  Vector<T> refColX;
+  if(!randominit ) {
+    for(int i=0; i<p; i++) {
+      X.refCol(i%n, refColX);
+      Z0.refCol(i%n, refColZ0);
+      refColZ0.copy(refColX);
+    }
+  } else {
+    for(int i=0; i<p; i++) {
+      int k = random() % n;
+      X.refCol(k, refColX);
+      Z0.refCol(i, refColZ0);
+      refColZ0.copy(refColX);
+    }
+  }
+  archRobustContinueForCombinedMemo(X, Z0, Z, I1, I2, warm, lambda2, epsilon, epsilon2);
+}
+
+template <typename T>
+void archetypalAnalysisContinue(const Matrix<T>& X, const Matrix<T>& Z0, Matrix<T>& Z, const bool robust = true, const T epsilon2 = T(10e-3), const bool computeXtX = false, const int stepsFISTA = 5, const int stepsAS = 50) {
+  bool warm = false;
+  T lambda2 = 1e-5;
+  T epsilon = 1e-5;
+  if (!robust) {
+    if (!computeXtX) 
+      archContinueForCombined(X, Z0, Z, stepsFISTA, stepsAS, warm, epsilon, epsilon2);
+    else
+      archContinueForCombinedMemo(X, Z0, Z, stepsFISTA, stepsAS, warm, epsilon, epsilon2);
+  } else {
+    if (!computeXtX)
+      archRobustContinueForCombined(X, Z0, Z, stepsFISTA, stepsAS, warm, epsilon, epsilon2);
+    else
+      archRobustContinueForCombinedMemo(X, Z0, Z, stepsFISTA, stepsAS, warm, epsilon, epsilon2);
+  }
+}
+
+template <typename T>
+void archetypalAnalysis(const Matrix<T>& X, const int p, Matrix<T>& Z, const bool robust = true, const T epsilon2 = T(10e-3), const bool computeXtX = false, const int stepsFISTA = 5, const int stepsAS = 50, const bool randominit = false) {
+  const int m = X.m();
+  const int n = X.n();
+  Matrix<T> Z0(m,p);
+  Vector<T> refColZ0;
+  Vector<T> refColX;
+  if(!randominit ) {
+    for(int i=0; i<p; i++) {
+      X.refCol(i%n, refColX);
+      Z0.refCol(i%n, refColZ0);
+      refColZ0.copy(refColX);
+    }
+  } else {
+    for(int i=0; i<p; i++) {
+      int k = random() % n;
+      X.refCol(k, refColX);
+      Z0.refCol(i, refColZ0);
+      refColZ0.copy(refColX);
+    }
+  }
+  archetypalAnalysisContinue(X, Z0, Z, robust, epsilon2, computeXtX, stepsFISTA, stepsAS);
+}
+
+template <typename T>
+void decompSimplex(const Matrix<T>& X, const Matrix<T>& Z, SpMatrix<T>& alpha, const bool computeZtZ = false) {
   const int n = X.n();
   const int p = Z.n();
   Matrix<T> AlphaT(p,n);
   Vector<T> refColX;
   Vector<T> refColAlphaT;
-  for(int i=0; i<n; ++i) {
-    X.refCol(i,refColX);
-    AlphaT.refCol(i, refColAlphaT);
-    activeSet(Z,refColX, refColAlphaT);
+  if(computeZtZ) {
+    Matrix<T> G;
+    Z.XtX(G);
+    T lambda2 = 1e-5;
+    G.addDiag(lambda2*lambda2);
+    for(int i=0; i<n; ++i) {
+      X.refCol(i,refColX);
+      AlphaT.refCol(i, refColAlphaT);
+      activeSetS(Z,refColX, refColAlphaT, G);
+    }
+    AlphaT.toSparse(alpha);
+  } else {
+    for(int i=0; i<n; ++i) {
+      X.refCol(i,refColX);
+      AlphaT.refCol(i, refColAlphaT);
+      activeSet(Z,refColX, refColAlphaT);
+    }
+    AlphaT.toSparse(alpha);
   }
-  AlphaT.toSparse(alpha);
 }
 
 #endif
