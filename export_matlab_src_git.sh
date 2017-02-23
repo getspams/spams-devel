27f9b8376f6a8c5de3a5a7638f459aa671a1810c
gdurif <gdurif@5c5caf25-3c07-451e-92dc-633d67ccb328>
2017-02-23 15:11:42 +0000

    rename .gitattributes_matlab to .gitattributes before git archive, and set back to .gitattributes_matlab after
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/spams-devel/trunk@350 5c5caf25-3c07-451e-92dc-633d67ccb328

--- export_matlab_src_git.sh
+++ export_matlab_src_git.sh
@@ -2,27 +2,17 @@
 
 ### Create the tar gzipped files with the Matlab and C++ sources
 
+# remove previous version if necessary
 rm -rf spams-matlab-v*
 
 # file name with date and version
 DA=$(``date +%F)
 echo $DA
 VERSION=`cat swig/Version`
-
 WDIR="spams-matlab-v$VERSION"
 WFILE="spams-matlab-v$VERSION-$da.tar.gz"
 
-# create the directory
-# svn export ./ spams-matlab
-git archive --format=tar.gz --prefix=$WDIR --output=$WFILE
-
-rm -rf spams-matlab/swig/
-rm spams-matlab/make_matlab_package.sh
-rm spams-matlab/TODO
-## make the archives
-DA=$(``date +%F)
-echo $DA
-
-VERSION=`cat swig/Version`
-tar -czf spams-matlab-v2.5-svn$da.tar.gz spams-matlab
-rm -rf spams-matlab
+# create the tar gzipped file
+mv .gitattributes_matlab .gitattributes
+git archive --format=tar.gz --prefix=$WDIR --output=$WFILE HEAD
+mv .gitattributes .gitattributes_matlab
