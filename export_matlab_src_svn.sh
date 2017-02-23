4e98d977cbba47024501b08a1e628882351caabc
gdurif <gdurif@5c5caf25-3c07-451e-92dc-633d67ccb328>
2017-02-23 15:11:41 +0000

    remove all unnecessary files before tar
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/spams-devel/trunk@349 5c5caf25-3c07-451e-92dc-633d67ccb328

--- export_matlab_src_svn.sh
+++ export_matlab_src_svn.sh
@@ -1,10 +1,37 @@
-rm -rf spams-matlab
-svn export ./ spams-matlab
-rm -rf spams-matlab/swig/
-rm spams-matlab/make_matlab_package.sh
-rm spams-matlab/TODO
-da=$(``date +%F)
-echo $da
-tar -czf spams-matlab-v2.5-svn$da.tar.gz spams-matlab
-rm -rf spams-matlab
+#!/bin/bash
 
+### Create the tar gzipped files with the Matlab and C++ sources
+
+# remove previous version if necessary
+rm -rf spams-matlab*
+
+# file name with date and version
+DA=$(``date +%F)
+echo $DA
+VERSION=`cat swig/Version`
+WDIR="spams-matlab-v$VERSION"
+WFILE="spams-matlab-v$VERSION-$da.tar.gz"
+
+# create the directory
+svn export ./ $WDIR
+
+# remove unnecessary files and directories
+rm -rf $WDIR/swig
+rm -rf $WDIR/wiki
+rm -f $WDIR/.gitattributes_matlab
+rm -f $WDIR/.gitattributes_cpp
+rm -f $WDIR/.gitignore
+rm -f $WDIR/compile_gcc.m
+rm -f $WDIR/compile_intel.m
+rm -f $WDIR/compile_mex_linux.m
+rm -f $WDIR/compile_mex_macosx.m
+rm -f $WDIR/export_matlab_src_git.sh
+rm -f $WDIR/export_matlab_src_svn.sh
+rm -f $WDIR/extract_matlab_src.sh
+rm -f $WDIR/make_matlab_linux.sh
+rm -f $WDIR/make_matlab_macosx.sh
+rm -f $WDIR/TODO
+
+# create the tar gzipped file
+tar -cvzf $WFILE $WDIR
+rm -rf spams-matlab
