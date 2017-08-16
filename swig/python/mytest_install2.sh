f5814eb6557b0bc44af90b67fcf3487510dfbb07
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-16 13:59:00 +0200

    new install directory

--- swig/python/mytest_install2.sh
+++ swig/python/mytest_install2.sh
@@ -3,15 +3,16 @@
 ## build python pkg
 
 ./clean
-./mkdist
+./mkdist -x
+WDIR=$(pwd)
 cd dist/spams-python
-inst=$HOME/python
-python setup.py install --prefix=$inst
+inst=$WDIR/python2-install
+python2 setup.py install --prefix=$inst
 
-PYV=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";` # get python current version
+PYV=`python2 -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";` # get python current version
 export PYTHONPATH=$inst/lib/python${PYV}/site-packages
 
 cd $inst/test
-python test_spams.py linalg
+python test_spams.py dictLearn
 
-cd $OLDPPWD
+cd $WDIR
