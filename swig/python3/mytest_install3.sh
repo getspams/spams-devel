9317b6a13f31b2a4ba05ab76010089ea34ec755e
Ghislain Durif <ghislain.durif@inria.fr>
2017-08-16 14:05:35 +0200

    new install directory

--- swig/python3/mytest_install3.sh
+++ swig/python3/mytest_install3.sh
@@ -2,15 +2,17 @@
 
 ## build python pkg
 
-./mkdist -py3
-cd dist/spams-python3
-inst=$HOME/python3
+./clean
+./mkdist -x
+WDIR=$(pwd)
+cd dist/spams-python
+inst=$WDIR/python3-install
 python3 setup.py install --prefix=$inst
 
 PYV=`python3 -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";` # get python current version
 export PYTHONPATH=$inst/lib/python${PYV}/site-packages
 
 cd $inst/test
-python3 test_spams.py linalg
+python3 test_spams.py
 
-cd $OLDPPWD
+cd $WDIR
