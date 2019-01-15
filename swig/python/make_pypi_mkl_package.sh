e02358a34a3eaca5ec7498f4f85346c562996efa
Ghislain Durif <ghislain.durif@inria.fr>
2019-01-15 15:38:15 +0100

    make spams mkl package and explain the installation in the doc

--- swig/python/make_pypi_mkl_package.sh
+++ swig/python/make_pypi_mkl_package.sh
@@ -3,13 +3,13 @@
 ## build python pkg
 
 ./clean
-./mkdist -x -m pypi -py3
+./mkdist -x -m pypi_mkl -py3
 
 
 echo "To upload the package: twine upload --repository-url <repos_url> dist/*"
 echo "<repos_url> can be https://test.pypi.org/legacy/ for PyPI test of https://upload.pypi.org/legacy/ for PyPI"
-echo "To install test version: pip3 install --index-url https://test.pypi.org/simple/ spams"
-echo "To install standard version: pip3 install spams"
+echo "To install test version: pip3 install --index-url https://test.pypi.org/simple/ spams_mkl"
+echo "To install standard version: pip3 install spams_mkl"
 
 ### on MacOS:
 ### env CC=/usr/local/bin/gcc-5 CXX=/usr/local/bin/g++-5 pip3 install --index-url https://test.pypi.org/simple/ spams
